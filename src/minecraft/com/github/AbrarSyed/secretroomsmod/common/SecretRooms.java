981bdf723ba0730cd76c3d5218982d61c3a2c995
AbrarSyed <sacabrarsyed@gmail.com>
2013-05-18 18:53:51 -0500

    added recipes for wooden buttons.. derp..

--- src/minecraft/com/github/AbrarSyed/secretroomsmod/common/SecretRooms.java
+++ src/minecraft/com/github/AbrarSyed/secretroomsmod/common/SecretRooms.java
@@ -3,6 +3,18 @@ package com.github.AbrarSyed.secretroomsmod.common;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
+
 import com.github.AbrarSyed.secretroomsmod.blocks.BlockCamoButton;
 import com.github.AbrarSyed.secretroomsmod.blocks.BlockCamoChest;
 import com.github.AbrarSyed.secretroomsmod.blocks.BlockCamoDoor;
@@ -30,17 +42,6 @@ import com.github.AbrarSyed.secretroomsmod.network.HandlerClient;
 import com.github.AbrarSyed.secretroomsmod.network.HandlerServer;
 import com.github.AbrarSyed.secretroomsmod.network.PacketSRMBase;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraftforge.common.Configuration;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.oredict.OreDictionary;
-import net.minecraftforge.oredict.ShapedOreRecipe;
-import net.minecraftforge.oredict.ShapelessOreRecipe;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -68,86 +69,86 @@ public class SecretRooms
 {
 
 	@SidedProxy(clientSide = "com.github.AbrarSyed.secretroomsmod.client.ProxyClient", serverSide = "com.github.AbrarSyed.secretroomsmod.common.ProxyCommon")
-	public static ProxyCommon	proxy;
+	public static ProxyCommon			proxy;
 
-	public static final String	MODID						= "secretroomsmod";
+	public static final String			MODID						= "secretroomsmod";
 
 	@Instance(value = MODID)
-	public static SecretRooms	instance;
+	public static SecretRooms			instance;
 
-	public static Logger		logger;
+	public static Logger				logger;
 
 	// textures
-	public static final String	TEXTURE_ITEM_PASTE			= MODID + ":CamoPaste";
-	public static final String	TEXTURE_ITEM_DOOR_WOOD		= MODID + ":CamoDoorWood";
-	public static final String	TEXTURE_ITEM_DOOR_STEEL		= MODID + ":CamoDoorSteel";
+	public static final String			TEXTURE_ITEM_PASTE			= MODID + ":CamoPaste";
+	public static final String			TEXTURE_ITEM_DOOR_WOOD		= MODID + ":CamoDoorWood";
+	public static final String			TEXTURE_ITEM_DOOR_STEEL		= MODID + ":CamoDoorSteel";
 
-	public static final String	TEXTURE_BLOCK_BASE			= MODID + ":CamoBase";
-	public static final String	TEXTURE_BLOCK_STAIR			= MODID + ":CamoStair";
-	public static final String	TEXTURE_BLOCK_CHEST			= MODID + ":CamoChest";
-	public static final String	TEXTURE_BLOCK_DETECTOR		= MODID + ":CamoDetector";
-	public static final String	TEXTURE_BLOCK_GATE			= MODID + ":CamoGate";
-	public static final String	TEXTURE_BLOCK_LEVER			= MODID + ":CamoLever";
-	public static final String	TEXTURE_BLOCK_REDSTONE		= MODID + ":CamoRedstone";
-	public static final String	TEXTURE_BLOCK_BUTTON		= MODID + ":CamoButton";
+	public static final String			TEXTURE_BLOCK_BASE			= MODID + ":CamoBase";
+	public static final String			TEXTURE_BLOCK_STAIR			= MODID + ":CamoStair";
+	public static final String			TEXTURE_BLOCK_CHEST			= MODID + ":CamoChest";
+	public static final String			TEXTURE_BLOCK_DETECTOR		= MODID + ":CamoDetector";
+	public static final String			TEXTURE_BLOCK_GATE			= MODID + ":CamoGate";
+	public static final String			TEXTURE_BLOCK_LEVER			= MODID + ":CamoLever";
+	public static final String			TEXTURE_BLOCK_REDSTONE		= MODID + ":CamoRedstone";
+	public static final String			TEXTURE_BLOCK_BUTTON		= MODID + ":CamoButton";
 
-	public static final String	TEXTURE_BLOCK_PLATE_PLAYER	= MODID + ":CamoPlatePlayer";
-	public static final String	TEXTURE_BLOCK_PLATE_WOOD	= MODID + ":CamoPlateWood";
-	public static final String	TEXTURE_BLOCK_PLATE_IRON	= MODID + ":CamoPlateIron";
-	public static final String	TEXTURE_BLOCK_PLATE_GOLD	= MODID + ":CamoPlateGold";
+	public static final String			TEXTURE_BLOCK_PLATE_PLAYER	= MODID + ":CamoPlatePlayer";
+	public static final String			TEXTURE_BLOCK_PLATE_WOOD	= MODID + ":CamoPlateWood";
+	public static final String			TEXTURE_BLOCK_PLATE_IRON	= MODID + ":CamoPlateIron";
+	public static final String			TEXTURE_BLOCK_PLATE_GOLD	= MODID + ":CamoPlateGold";
 
-	public static final String	TEXTURE_BLOCK_TORCH			= MODID + ":TorchLever";
+	public static final String			TEXTURE_BLOCK_TORCH			= MODID + ":TorchLever";
 
-	public static final String	TEXTURE_BLOCK_SOLID_AIR		= MODID + ":SolidAir";
-	public static final String	TEXTURE_BLOCK_CLEAR			= MODID + ":clear";
+	public static final String			TEXTURE_BLOCK_SOLID_AIR		= MODID + ":SolidAir";
+	public static final String			TEXTURE_BLOCK_CLEAR			= MODID + ":clear";
 
 	// render IDs
-	public static boolean		displayCamo					= true;
-	public static int			render3DId;
-	public static int			renderFlatId;
+	public static boolean				displayCamo					= true;
+	public static int					render3DId;
+	public static int					renderFlatId;
 
 	// misc
-	public static Block			torchLever;
-	public static Block			oneWay;
+	public static Block					torchLever;
+	public static Block					oneWay;
 
 	// doors and Trap-Doors
-	public static Block			camoTrapDoor;
-	public static Block			camoDoorWood;
-	public static Item			camoDoorWoodItem;
-	public static Block			camoDoorIron;
-	public static Item			camoDoorIronItem;
+	public static Block					camoTrapDoor;
+	public static Block					camoDoorWood;
+	public static Item					camoDoorWoodItem;
+	public static Block					camoDoorIron;
+	public static Item					camoDoorIronItem;
 
 	// Camo Paste
-	public static Item			camoPaste;
+	public static Item					camoPaste;
 
 	// FullCamo Stuff
-	public static Block			camoGhost;
-	public static Block			camoLever;
-	public static Block			camoCurrent;
-	public static Block			camoButton;
-	public static Block			camoGate;
-	public static Block			camoGateExt;
-	public static Block			camoPlateAll;
-	public static Block			camoPlatePlayer;
-	public static Block			camoPlateLight;
-	public static Block			camoPlateHeavy;
-	public static Block			camoStairs;
-	public static Block			camoChest;
-	public static Block			camoTrappedChest;
-	public static Block			camoLightDetector;
+	public static Block					camoGhost;
+	public static Block					camoLever;
+	public static Block					camoCurrent;
+	public static Block					camoButton;
+	public static Block					camoGate;
+	public static Block					camoGateExt;
+	public static Block					camoPlateAll;
+	public static Block					camoPlatePlayer;
+	public static Block					camoPlateLight;
+	public static Block					camoPlateHeavy;
+	public static Block					camoStairs;
+	public static Block					camoChest;
+	public static Block					camoTrappedChest;
+	public static Block					camoLightDetector;
 
-	public static Block			solidAir;
+	public static Block					solidAir;
 
-	public static final String	CAMO_PASTE					= "camoPaste";
+	public static final String			CAMO_PASTE					= "camoPaste";
 
 	// creative tab
-	public static CreativeTabs	tab;
-	
+	public static CreativeTabs			tab;
+
 	// material
-	public static final MaterialFakeAir AIR_MAT = new MaterialFakeAir(); 
+	public static final MaterialFakeAir	AIR_MAT						= new MaterialFakeAir();
 
 	// ids
-	private int[]				ids;
+	private int[]						ids;
 
 	@PreInit
 	public void preLoad(FMLPreInitializationEvent e)
@@ -254,8 +255,8 @@ public class SecretRooms
 		GameRegistry.registerBlock(camoGhost, "GhostBlock");
 		GameRegistry.registerBlock(camoLever, "SecretCamoLever");
 		GameRegistry.registerBlock(camoCurrent, "SecretCamoRedstone");
-		
-		GameRegistry.registerBlock(camoButton, ItemBlockCamoButton.class,  "SecretCamoButton");
+
+		GameRegistry.registerBlock(camoButton, ItemBlockCamoButton.class, "SecretCamoButton");
 
 		GameRegistry.registerBlock(camoPlateAll, "SecretPressurePlate");
 		GameRegistry.registerBlock(camoPlatePlayer, "SecretPlayerPlate");
@@ -292,7 +293,7 @@ public class SecretRooms
 		LanguageRegistry.instance().addNameForObject(camoLever, "en_US", "Secret Lever");
 		LanguageRegistry.instance().addNameForObject(camoCurrent, "en_US", "Secret Redstone");
 		LanguageRegistry.instance().addNameForObject(camoButton, "en_US", "Secret Button");
-		
+
 		LanguageRegistry.instance().addStringLocalization("mod_SRM.SecretButton.stone", "Secret Stone Button");
 		LanguageRegistry.instance().addStringLocalization("mod_SRM.SecretButton.wood", "Secret Wood Button");
 		LanguageRegistry.instance().addStringLocalization("mod_SRM.SecretButton.stone", "en_us", "Secret Stone Button");
@@ -482,8 +483,8 @@ public class SecretRooms
 				'@', Block.lever
 		}));
 
-		// Camo-Button stuff
-		recipes.add(new ShapedOreRecipe(new ItemStack(camoButton, 4), new Object[] {
+		// Camo-Button Stone stuff
+		recipes.add(new ShapedOreRecipe(new ItemStack(camoButton, 4, 0), new Object[] {
 				"X0X",
 				"0@0",
 				"X0X",
@@ -491,13 +492,32 @@ public class SecretRooms
 				'0', Item.rottenFlesh,
 				'@', Block.stoneButton
 		}));
-		recipes.add(new ShapedOreRecipe(new ItemStack(camoButton, 4), new Object[] {
+		recipes.add(new ShapedOreRecipe(new ItemStack(camoButton, 4, 0), new Object[] {
 				"X0X",
 				"0@0",
 				"X0X",
 				'X', CAMO_PASTE,
 				'0', new ItemStack(Block.cloth.blockID, 1, OreDictionary.WILDCARD_VALUE),
-				'@', Block.stoneButton }));
+				'@', Block.stoneButton
+		}));
+
+		// Camo-Button Wood stuff
+		recipes.add(new ShapedOreRecipe(new ItemStack(camoButton, 4, 1), new Object[] {
+				"X0X",
+				"0@0",
+				"X0X",
+				'X', CAMO_PASTE,
+				'0', Item.rottenFlesh,
+				'@', Block.woodenButton
+		}));
+		recipes.add(new ShapedOreRecipe(new ItemStack(camoButton, 4, 1), new Object[] {
+				"X0X",
+				"0@0",
+				"X0X",
+				'X', CAMO_PASTE,
+				'0', new ItemStack(Block.cloth.blockID, 1, OreDictionary.WILDCARD_VALUE),
+				'@', Block.woodenButton
+		}));
 
 		// pressure plates
 		recipes.add(new ShapedOreRecipe(new ItemStack(camoPlateAll, 1), new Object[] {
