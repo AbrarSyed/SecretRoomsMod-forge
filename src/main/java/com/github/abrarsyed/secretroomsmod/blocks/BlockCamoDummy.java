172d1f34979a5d511208a8a0150ce2f34ddb9274
AbrarSyed <sacabrarsyed@gmail.com>
2014-04-15 12:08:14 -0500

    chamged the package to lowercase only

---
+++ src/main/java/com/github/abrarsyed/secretroomsmod/blocks/BlockCamoDummy.java
@@ -0,0 +1,37 @@
+package com.github.abrarsyed.secretroomsmod.blocks;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
+/**
+ * @author AbrarSyed
+ */
+public class BlockCamoDummy extends BlockCamoFull
+{
+	public BlockCamoDummy()
+	{
+		super();
+		setHardness(2.5F);
+		setStepSound(Block.soundTypeWood);
+		setCreativeTab(null);
+	}
+
+	@Override
+	public int quantityDropped(Random random)
+	{
+		return 0;
+	}
+	
+    public Item getItemDropped(int i, Random random, int j)
+    {
+        return null;
+    }
+
+	@Override
+	public int getMobilityFlag()
+	{
+		return 2;
+	}
+}
---
+++ src/main/java/com/github/abrarsyed/secretroomsmod/client/ProxyClient.java
@@ -0,0 +1,69 @@
+package com.github.abrarsyed.secretroomsmod.client;
+
+import java.util.UUID;
+
+import net.minecraft.client.settings.KeyBinding;
+
+import org.lwjgl.input.Keyboard;
+
+import com.github.abrarsyed.secretroomsmod.common.ProxyCommon;
+import com.github.abrarsyed.secretroomsmod.common.SecretRooms;
+import com.github.abrarsyed.secretroomsmod.network.PacketKey;
+import com.github.abrarsyed.secretroomsmod.network.PacketManager;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.event.FMLServerStoppingEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(value = Side.CLIENT)
+public class ProxyClient extends ProxyCommon
+{
+	public static KeyBinding	key_OneWayFace;
+	private boolean				oneWayFaceTowards	= true;
+
+	public ProxyClient()
+	{
+		key_OneWayFace = new KeyBinding("key.secretroomsmod.oneWayface", Keyboard.KEY_BACKSLASH, "key.categories.gameplay");
+		//MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@Override
+	public void loadRenderStuff()
+	{
+		SecretRooms.render3DId = RenderingRegistry.getNextAvailableRenderId();
+		SecretRooms.renderFlatId = RenderingRegistry.getNextAvailableRenderId();
+		RenderingRegistry.registerBlockHandler(new BlockRenderer(SecretRooms.render3DId));
+		RenderingRegistry.registerBlockHandler(new BlockRenderer(SecretRooms.renderFlatId));
+	}
+
+	@Override
+	public void loadKeyStuff()
+	{
+		ClientRegistry.registerKeyBinding(key_OneWayFace);
+		FMLCommonHandler.instance().bus().register(new SecretKeyHandler());
+	}
+
+	@Override
+	public void onServerStop(FMLServerStoppingEvent e)
+	{
+		super.onServerStop(e);
+		oneWayFaceTowards = true;
+	}
+
+	@Override
+	public void onKeyPress(UUID uuid)
+	{
+		oneWayFaceTowards = !oneWayFaceTowards;
+		PacketManager.sendToServer(new PacketKey());
+	}
+
+	@Override
+	public boolean getFaceTowards(UUID uuid)
+	{
+		return oneWayFaceTowards;
+	}
+
+}
