87f067a4736103a4a82924a5704b19f330cc4a18
AbrarSyed <sacabrarsyed@gmail.com>
2014-05-27 21:42:30 -0500

    fixed packet system to not cause memory leaks

--- src/main/java/com/github/abrarsyed/secretroomsmod/network/PacketManager.java
+++ src/main/java/com/github/abrarsyed/secretroomsmod/network/PacketManager.java
@@ -5,6 +5,7 @@ import static cpw.mods.fml.relauncher.Side.SERVER;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.lang.reflect.Constructor;
 import java.util.EnumMap;
@@ -17,7 +18,6 @@ import net.minecraft.network.Packet;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import com.google.common.collect.Maps;
 import com.google.common.io.ByteArrayDataInput;
@@ -33,74 +33,50 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @Sharable
-public class PacketManager extends FMLIndexedMessageToMessageCodec<PacketBase>
+public class PacketManager
 {
-    private static final Logger        LOGGER   = LogManager.getLogger();
-    private static final PacketManager INSTANCE = new PacketManager();
     private static final EnumMap<Side, FMLEmbeddedChannel> channels = Maps.newEnumMap(Side.class);
 
     public static void init()
     {
         if (!channels.isEmpty()) // avoid duplicate inits..
             return;
-        
-        INSTANCE.addDiscriminator(0, PacketCamo.class);
-        INSTANCE.addDiscriminator(1, PacketShowToggle.class);
-        INSTANCE.addDiscriminator(2, PacketKey.class);
-        INSTANCE.addDiscriminator(3, PacketSyncOwnership.class);
-        INSTANCE.addDiscriminator(4, PacketChangeOwnership.class);
 
-        channels.putAll(NetworkRegistry.INSTANCE.newChannel("SecretRooms", INSTANCE));
-    }
-    
-    // IO METHODS
+        Codec codec = new Codec();
 
-    @Override
-    public void encodeInto(ChannelHandlerContext ctx, PacketBase packet, ByteBuf target) throws Exception
-    {
-        ByteArrayDataOutput output = ByteStreams.newDataOutput();
-        packet.encode(output);
-        target.writeBytes(output.toByteArray());
-    }
+        codec.addDiscriminator(0, PacketCamo.class);
+        codec.addDiscriminator(1, PacketShowToggle.class);
+        codec.addDiscriminator(2, PacketKey.class);
+        codec.addDiscriminator(3, PacketSyncOwnership.class);
+        codec.addDiscriminator(4, PacketChangeOwnership.class);
 
-    @Override
-    public void decodeInto(ChannelHandlerContext ctx, ByteBuf source, PacketBase packet)
-    {
-        ByteArrayDataInput input = ByteStreams.newDataInput(source.array());
-        input.skipBytes(1); // skip the packet identifier byte
-        packet.decode(input);
+        channels.putAll(NetworkRegistry.INSTANCE.newChannel("SecretRooms", codec));
 
-        if (FMLCommonHandler.instance().getEffectiveSide().isClient())
+        // add handlers
+        if (FMLCommonHandler.instance().getSide().isClient())
         {
-            actionClient(packet);
+            // for the client
+            FMLEmbeddedChannel channel = channels.get(Side.CLIENT);
+            String codecName = channel.findChannelHandlerNameForType(Codec.class);
+            channel.pipeline().addAfter(codecName, "ClientHandler", new HandlerClient());
         }
         else
         {
-            actionServer(ctx, packet);
+            // for the server
+            FMLEmbeddedChannel channel = channels.get(Side.SERVER);
+            String codecName = channel.findChannelHandlerNameForType(Codec.class);
+            channel.pipeline().addAfter(codecName, "ServerHandler", new HandlerServer());
         }
     }
-    
-    @SideOnly(Side.CLIENT)
-    private void actionClient(PacketBase packet)
-    {
-        Minecraft mc = Minecraft.getMinecraft();
-        packet.actionClient(mc.theWorld, mc.thePlayer);
-    }
-    
-    private void actionServer(ChannelHandlerContext ctx, PacketBase packet)
-    {
-        EntityPlayerMP player = ((NetHandlerPlayServer)ctx.channel().attr(NetworkRegistry.NET_HANDLER).get()).playerEntity;
-        packet.actionServer(player.worldObj, player);
-    }
-    
+
     // UTIL SENDING METHODS
-    
+
     public static void sendToServer(PacketBase packet)
     {
         channels.get(CLIENT).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.TOSERVER);
         channels.get(CLIENT).writeAndFlush(packet);
     }
-    
+
     public static void sendToPlayer(PacketBase packet, EntityPlayer player)
     {
         channels.get(SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.PLAYER);
@@ -121,60 +97,119 @@ public class PacketManager extends FMLIndexedMessageToMessageCodec<PacketBase>
         channels.get(SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(dimension);
         channels.get(SERVER).writeAndFlush(packet);
     }
-    
+
     public static void sendToAll(PacketBase packet)
     {
         channels.get(SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.ALL);
         channels.get(SERVER).writeAndFlush(packet);
     }
-    
+
     public static Packet toMcPacket(PacketBase packet)
     {
         return channels.get(FMLCommonHandler.instance().getEffectiveSide()).generatePacketFrom(packet);
     }
-    
-    // OTHER METHODS
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+    /**
+     * @author abrarsyed
+     *         This class converts FMLMessage to PacketBase for my system to handle.
+     */
+    private static final class Codec extends FMLIndexedMessageToMessageCodec<PacketBase>
     {
-        LOGGER.log(Level.ERROR, "Error in packet handling!", cause);
-        ctx.fireExceptionCaught(cause); // for the other handlers.
-    }
-
-    @Override
-    public FMLIndexedMessageToMessageCodec<PacketBase> addDiscriminator(int discriminator, Class<? extends PacketBase> type)
-    {
-        // double check it has an empty constructor. or fail early.
-        // This is to gaurd myself against my own stupidity.
-
-        if (!hasEmptyContructor(type))
+        @Override
+        public void encodeInto(ChannelHandlerContext ctx, PacketBase packet, ByteBuf target) throws Exception
         {
-            LOGGER.log(Level.FATAL, type.getName() + "does not have an empty constructor!");
+            ByteArrayDataOutput output = ByteStreams.newDataOutput();
+            packet.encode(output);
+            target.writeBytes(output.toByteArray());
         }
 
-        return super.addDiscriminator(discriminator, type);
-    }
-
-    @SuppressWarnings("rawtypes")
-    private static boolean hasEmptyContructor(Class type)
-    {
-        try
+        @Override
+        public void decodeInto(ChannelHandlerContext ctx, ByteBuf source, PacketBase packet)
         {
-            for (Constructor c : type.getConstructors())
+            ByteArrayDataInput input = ByteStreams.newDataInput(source.array());
+            input.skipBytes(1); // skip the packet identifier byte
+            packet.decode(input);
+
+            if (FMLCommonHandler.instance().getEffectiveSide().isClient())
             {
-                if (c.getParameterTypes().length == 0)
-                {
-                    return true;
-                }
+                actionClient(packet);
+            }
+            else
+            {
+                actionServer(ctx, packet);
             }
         }
-        catch (SecurityException e)
+
+        @SideOnly(Side.CLIENT)
+        private void actionClient(PacketBase packet)
         {
-            // really?
+            Minecraft mc = Minecraft.getMinecraft();
+            packet.actionClient(mc.theWorld, mc.thePlayer);
         }
 
-        return false;
+        private void actionServer(ChannelHandlerContext ctx, PacketBase packet)
+        {
+            EntityPlayerMP player = ((NetHandlerPlayServer) ctx.channel().attr(NetworkRegistry.NET_HANDLER).get()).playerEntity;
+            packet.actionServer(player.worldObj, player);
+        }
+
+        @Override
+        public FMLIndexedMessageToMessageCodec<PacketBase> addDiscriminator(int discriminator, Class<? extends PacketBase> type)
+        {
+            // double check it has an empty constructor. or fail early.
+            // This is to guard myself against my own stupidity.
+
+            if (!hasEmptyContructor(type))
+            {
+                LogManager.getLogger().log(Level.FATAL, type.getName() + "does not have an empty constructor!");
+            }
+
+            return super.addDiscriminator(discriminator, type);
+        }
+
+        @SuppressWarnings("rawtypes")
+        private static boolean hasEmptyContructor(Class type)
+        {
+            try
+            {
+                for (Constructor c : type.getConstructors())
+                {
+                    if (c.getParameterTypes().length == 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (SecurityException e)
+            {
+                // really?
+            }
+
+            return false;
+        }
+    }
+
+    @Sharable
+    @SideOnly(Side.CLIENT)
+    private static final class HandlerClient extends SimpleChannelInboundHandler<PacketBase>
+    {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, PacketBase packet) throws Exception
+        {
+            Minecraft mc = Minecraft.getMinecraft();
+            packet.actionClient(mc.theWorld, mc.thePlayer);
+        }
+    }
+
+    @Sharable
+    private static final class HandlerServer extends SimpleChannelInboundHandler<PacketBase>
+    {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, PacketBase packet) throws Exception
+        {
+            EntityPlayerMP player = ((NetHandlerPlayServer) ctx.channel().attr(NetworkRegistry.NET_HANDLER).get()).playerEntity;
+            packet.actionServer(player.worldObj, player);
+        }
     }
 
 }
