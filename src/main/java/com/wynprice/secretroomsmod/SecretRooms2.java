647b54482fd0f361545b547ab1b15ca99e458ce6
Wyn Price <wynprice999@gmail.com>
2017-11-02 02:19:22 +0000

    allowed build.gradlew to change version

--- src/main/java/com/wynprice/secretroomsmod/SecretRooms2.java
+++ src/main/java/com/wynprice/secretroomsmod/SecretRooms2.java
@@ -25,7 +25,7 @@ public class SecretRooms2
 {
     public static final String MODID = "secretroomsmod";
     public static final String MODNAME = "Secret Rooms 2";
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "@VERSION@";
     
     @SidedProxy(modId = MODID, clientSide = "com.wynprice.secretroomsmod.proxy.ClientProxy", serverSide = "com.wynprice.secretroomsmod.proxy.ServerProxy")
     public static CommonProxy proxy;
