970c8570a90a81a69ed3e33e7289b27b0e589c07
Wyn Price <wynprice999@gmail.com>
2018-01-20 14:38:30 +0000

    updated version

--- src/main/java/com/wynprice/secretroomsmod/SecretRooms5.java
+++ src/main/java/com/wynprice/secretroomsmod/SecretRooms5.java
@@ -35,7 +35,7 @@ public class SecretRooms5
 {
     public static final String MODID = "secretroomsmod";
     public static final String MODNAME = "Secret Rooms 5";
-    public static final String VERSION = "5.2.1";
+    public static final String VERSION = "5.2.2";
     public static final String UPDATE_URL = "http://www.wynprice.com/update_jsons/secretroomsmod.json";
 	
     
