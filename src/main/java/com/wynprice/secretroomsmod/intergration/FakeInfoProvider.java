d79a9ae23bb09e9a96aa0f726499fdaee1274f8d
Wyn Price <wynprice999@gmail.com>
2018-01-21 22:02:14 +0000

    Added the one probe support for Energized Paste

--- src/main/java/com/wynprice/secretroomsmod/intergration/FakeInfoProvider.java
+++ src/main/java/com/wynprice/secretroomsmod/intergration/FakeInfoProvider.java
@@ -2,6 +2,7 @@ package com.wynprice.secretroomsmod.intergration;
 
 import com.wynprice.secretroomsmod.base.interfaces.ISecretBlock;
 import com.wynprice.secretroomsmod.base.interfaces.ISecretTileEntity;
+import com.wynprice.secretroomsmod.handler.EnergizedPasteHandler;
 import com.wynprice.secretroomsmod.items.TrueSightHelmet;
 
 import mcjty.theoneprobe.Tools;
@@ -22,42 +23,70 @@ import net.minecraft.world.World;
 
 public class FakeInfoProvider implements IBlockDisplayOverride
 {
-
+	
 	@Override
 	public boolean overrideStandardInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world,
 			IBlockState blockState, IProbeHitData data) 
 	{
-		if(!(blockState.getBlock() instanceof ISecretBlock))
-			return false;
-		DefaultProbeInfoProvider.showStandardBlockInfo(new ProbeConfig(), mode, probeInfo, player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? 
-				blockState : ISecretTileEntity.getMirrorState(world, data.getPos()), player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? 
-				blockState.getBlock() : ISecretTileEntity.getMirrorState(world, data.getPos()).getBlock(), world, data.getPos(), player, new IProbeHitData() {
-					
-					@Override
-					public EnumFacing getSideHit() {
-						return data.getSideHit();
-					}
-					
-					@Override
-					public BlockPos getPos() {
-						return data.getPos();
-					}
-					
-					@Override
-					public ItemStack getPickBlock() {
-						return player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? data.getPickBlock() : 
-							ISecretTileEntity.getMirrorState(world, data.getPos()).getBlock()
-								.getPickBlock(ISecretTileEntity.getMirrorState(world, data.getPos()), 
-										world.rayTraceBlocks(player.getPositionVector(), player.getPositionVector()
-												.addVector(player.getLookVec().x * 5, player.getLookVec().y * 5, player.getLookVec().z * 5)), world, data.getPos(), player);
-					}
-					
-					@Override
-					public Vec3d getHitVec() {
-						return data.getHitVec();
-					}
-				});
-		return true;
+		if(blockState.getBlock() instanceof ISecretBlock)
+			DefaultProbeInfoProvider.showStandardBlockInfo(new ProbeConfig(), mode, probeInfo, player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? 
+					blockState : ISecretTileEntity.getMirrorState(world, data.getPos()), player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? 
+					blockState.getBlock() : ISecretTileEntity.getMirrorState(world, data.getPos()).getBlock(), world, data.getPos(), player, new IProbeHitData() {
+						
+						@Override
+						public EnumFacing getSideHit() {
+							return data.getSideHit();
+						}
+						
+						@Override
+						public BlockPos getPos() {
+							return data.getPos();
+						}
+						
+						@Override
+						public ItemStack getPickBlock() {
+							return player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? data.getPickBlock() : 
+								ISecretTileEntity.getMirrorState(world, data.getPos()).getBlock()
+									.getPickBlock(ISecretTileEntity.getMirrorState(world, data.getPos()), 
+											world.rayTraceBlocks(player.getPositionVector(), player.getPositionVector()
+													.addVector(player.getLookVec().x * 5, player.getLookVec().y * 5, player.getLookVec().z * 5)), world, data.getPos(), player);
+						}
+						
+						@Override
+						public Vec3d getHitVec() {
+							return data.getHitVec();
+						}
+					});
+		if(EnergizedPasteHandler.hasReplacedState(world, data.getPos()))
+			DefaultProbeInfoProvider.showStandardBlockInfo(new ProbeConfig(), mode, probeInfo, player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? 
+					blockState : EnergizedPasteHandler.getReplacedState(world, data.getPos()), player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? 
+					blockState.getBlock() : EnergizedPasteHandler.getReplacedState(world, data.getPos()).getBlock(), world, data.getPos(), player, new IProbeHitData() {
+						
+						@Override
+						public EnumFacing getSideHit() {
+							return data.getSideHit();
+						}
+						
+						@Override
+						public BlockPos getPos() {
+							return data.getPos();
+						}
+						
+						@Override
+						public ItemStack getPickBlock() {
+							return player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof TrueSightHelmet ? data.getPickBlock() : 
+								EnergizedPasteHandler.getReplacedState(world, data.getPos()).getBlock()
+									.getPickBlock(EnergizedPasteHandler.getReplacedState(world, data.getPos()), 
+											world.rayTraceBlocks(player.getPositionVector(), player.getPositionVector()
+													.addVector(player.getLookVec().x * 5, player.getLookVec().y * 5, player.getLookVec().z * 5)), world, data.getPos(), player);
+						}
+						
+						@Override
+						public Vec3d getHitVec() {
+							return data.getHitVec();
+						}
+					});
+		return blockState.getBlock() instanceof ISecretBlock || EnergizedPasteHandler.hasReplacedState(world, data.getPos());
 	}
 
 }
