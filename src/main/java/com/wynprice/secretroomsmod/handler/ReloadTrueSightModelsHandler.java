0d649830aeced867b194d0eb7242e6812003ca6e
Wyn Price <wynprice999@gmail.com>
2018-01-21 22:09:29 +0000

    Added True Sight Helmet support for Energized Paste

--- src/main/java/com/wynprice/secretroomsmod/handler/ReloadTrueSightModelsHandler.java
+++ src/main/java/com/wynprice/secretroomsmod/handler/ReloadTrueSightModelsHandler.java
@@ -3,12 +3,11 @@ package com.wynprice.secretroomsmod.handler;
 import com.wynprice.secretroomsmod.base.interfaces.ISecretBlock;
 import com.wynprice.secretroomsmod.base.interfaces.ISecretTileEntity;
 import com.wynprice.secretroomsmod.items.TrueSightHelmet;
-import com.wynprice.secretroomsmod.tileentity.TileEntityInfomationHolder;
 
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
@@ -28,6 +27,9 @@ public class ReloadTrueSightModelsHandler
 			for(TileEntity holder : ISecretBlock.ALL_SECRET_TILE_ENTITIES)
 				if(holder instanceof ISecretTileEntity)
 					event.player.world.markBlockRangeForRenderUpdate(holder.getPos().add(-1, -1, -1), holder.getPos().add(1, 1, 1));
+			for(BlockPos pos : EnergizedPasteHandler.getEnergized_map().get(event.player.world.provider.getDimension()).keySet())
+				event.player.world.markBlockRangeForRenderUpdate(pos.add(-1, -1, -1), pos.add(1, 1, 1));
+
 		}
 	}
 }
